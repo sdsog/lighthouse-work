4ebba94c6c64b5ffaf9ce8c5bde834e47a510698
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-31 21:18:36 +0000

    added testing

---
+++ w1d5/creating-exporting-modules/make-array.js
@@ -0,0 +1,25 @@
+
+var newArr = [];
+
+var addToList = function(num) {
+		newArr.push(num);
+		return newArr;
+};
+		
+var sortedArr = function(num)
+		
+
+// var sortArr = function(arr) {
+// 		var sortedArr = arr.sort(function(a,b) {
+// 		return (a - b);
+// 	});
+
+// 	};
+
+
+console.log(addToList(8));
+console.log(addToList(7));
+console.log(addToList(6));
+console.log("this is newArr " + newArr);
+
+// console.log("this is sortArr: " + sortArr(newArr));
---
+++ w1d5/creating-exporting-modules/node.js
@@ -0,0 +1,10 @@
+
+var inputNumbers = process.argv.slice(2);
+console.log('inputNumbers: ' + inputNumbers);
+
+console.log(typeof inputNumbers);
+
+
+var input = require("./make-array");
+
+console.log(input.)
