16322135bc8cc400357db458391097e4d18759f9
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-31 22:36:29 +0000

    added palindrome

---
+++ w1d5/running-tests-with-modules/web-js-palindromes/lib/palindromes.js
@@ -0,0 +1,10 @@
+function isPalindrome(s) {
+  s = s.replace(/\s/g, '');
+  var stringReverse = s.split("").reverse().join("");
+  return s == stringReverse;
+}
+
+module.exports = isPalindrome;
+
+
+
