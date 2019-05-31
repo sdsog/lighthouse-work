148fba6d0dbee64fe24e9c3fc56a40087fd50039
sdsog <34170634+sdsog@users.noreply.github.com>
2019-05-31 00:34:16 +0000

    added countdown exercise

--- w1d4/using-closures/countdown-generator.js
+++ w1d4/using-closures/countdown-generator.js
@@ -14,12 +14,8 @@ var countdownGenerator = function (x) {
 
   	id -= 1;
 
-  	if (id === 3) {
-		console.log("T minus 3...");
-	} else if (id === 2) {
-		console.log("T minus 2...");
-	} else if (id === 1) {
-		console.log("T minus 1...");
+  	if (id > 0) {
+		console.log("T minus " + id + "...");
 	} else if (id === 0) {
 		console.log("Blast Off!");
 	} else {
@@ -33,10 +29,16 @@ var countdownGenerator = function (x) {
 
 };
 
-var countdown = countdownGenerator(3);
+var countdown = countdownGenerator(6);
 countdown(); // T-minus 3...
 countdown(); // T-minus 2...
 countdown(); // T-minus 1...
 countdown(); // Blast Off!
 countdown(); // Rockets already gone, bub!
 countdown(); // Rockets already gone, bub!
+countdown();
+countdown();
+countdown();
+countdown();
+countdown();
+countdown();
